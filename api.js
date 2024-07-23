634eba41fbe7afa202c66cfa1eae8b62e4905f1e
damian <dg997678@gmail.com>
2024-07-23 11:03:10 -0500

    add img and styles api.js

--- api.js
+++ api.js
@@ -8,12 +8,14 @@ async function apiUsers() {
 
         data.forEach(anime => {
             const $animeContainer = document.createElement('DIV');
+            $animeContainer.classList.add('w-80', 'h-[500px]', 'border', 'border-gray-300', 'p-4', 'flex', 'flex-col',
+                 'items-center', 'bg-white', 'shadow-lg', 'rounded-lg');
             $animeContainer.innerHTML = /*html*/`
-            <div class="flex flex-col">
-                <h1>${anime.title}</h1>
-                <strong>Genre: ${anime.genre}</strong>
-                <strong>Studio: ${anime.studioId}</strong>
-                <img src="http://localhost:3000${anime.image}" alt="${anime.title}">
+            <div class="flex flex-col items-center justify-between h-full w-full">
+                <img src="http://localhost:3000${anime.image}" alt="${anime.title}" class="w-full h-3/4 object-cover mb-4 rounded-md">
+                <h1 class="text-xl font-bold mb-2">${anime.title}</h1>
+                <strong class="text-sm">Genre: ${anime.genre}</strong>
+                <strong class="text-sm">Studio: ${anime.studioId}</strong>
             </div>
            `;
             $container.appendChild($animeContainer);
